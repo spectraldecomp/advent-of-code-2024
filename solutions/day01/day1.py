841fd517f5f2a1b922d069aad04cf01cfcaad77f
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2024-11-30 22:45:32 -0600

    Reformatting

--- solutions/day01/day1.py
+++ solutions/day01/day1.py
@@ -1,26 +1,6 @@
-input_lines = open('solutions/day1/input.txt').read().splitlines()
-sum = 0
-for line in input_lines:
-    num = []
-    keywords = {'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine'}
+import numpy as np
+import sys
 
-    # replace all occurences of keywords with their corresponding values
-    for key in keywords:
-        line = line.replace(key, keywords[key])
-                        
-    # check from the left
-    for i in range(len(line)):
-        if line[i].isdigit():
-            num.append(line[i])
-            break
-    
-    # check from the right
-    for i in range(len(line)-1, -1, -1):
-        if line[i].isdigit():
-            num.append(line[i])
-            break
-    # convert list to string
-    num = ''.join(num)
-    sum += int(num)
-print(sum)
-    
+# lines = open('solutions/day01/sample.txt').read().splitlines()
+lines = open('solutions/day01/input.txt').read().splitlines()
+arr = np.array([list(line) for line in lines])
