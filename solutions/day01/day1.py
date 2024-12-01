bf864bde623dc2745b3297c60b9fdb3ce3669380
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2024-11-30 23:11:36 -0600

    day 1

--- solutions/day01/day1.py
+++ solutions/day01/day1.py
@@ -1,6 +1,27 @@
 import numpy as np
-import sys
 
-# lines = open('solutions/day01/sample.txt').read().splitlines()
+lines = open('solutions/day01/sample.txt').read().splitlines()
 lines = open('solutions/day01/input.txt').read().splitlines()
-arr = np.array([list(line) for line in lines])
+
+left = []
+right = []
+for line in lines:
+    left.append(int(line.split()[0]))
+    right.append(int(line.split()[1]))
+left.sort()
+right.sort()
+
+# day 1
+differences = []
+for i in range(len(left)):
+    differences.append(abs(left[i] - right[i]))
+    
+print(sum(differences))
+
+# day 2
+total = 0
+for number in left:
+    freq = right.count(number)
+    total += freq * number
+    
+print(total)
